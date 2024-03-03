bc245cae8a386ee27da3ce789f6a203023a1f627
waves-s <102697447+waves-s@users.noreply.github.com>
2024-03-03 01:13:59 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ st.sidebar.title('No-Code Machine Learning Prediction Model for Non-Techies')
 
 st.subheader(":blue[How to Use:]")
 # Purpose
-st.markdown(""":blue[Purpose of this App:] The aim of this app is to help users identify trend in data worth exploring without coding or machine learning knowledge/experience. It should be noted that this is a basic machine learning model. Further analysis is needed to address bias, overfitting, underfitting, and ensure dataset is balanced for classification problems.""")
+st.markdown(""":blue[Purpose of this App:] The aim of this app is to help users identify if trend exist in data worth exploring without coding or machine learning knowledge/experience. It should be noted that this is a basic machine learning model. Further analysis is needed to address bias, overfitting, underfitting, and ensure dataset is balanced for classification problems.""")
 
 
 # Step 1: Clean Data
-st.markdown(""" :blue[Clean Data:] Remove errors, duplicates, and unnecessary columns. Handle missing values. Ensure each column contains either numbers or text. Mixing text and numbers will cause errors. For classification problems, ensure your target column contains balanced classes.""")
+st.markdown(""" :blue[Clean Data:] Remove errors, duplicates, and unnecessary columns. Handle missing values. Ensure each column contains either numbers or text. Mixing text and numbers will cause errors. For classification tasks, ensure the target column exhibits balanced classes i.e. each class is represented proportionally to real-world prevelence. e.g. if population is evenly split between genders or if certain classes are rare like people over 6 feet tall, then the dataset should reflect that.""")
 
 # Step 2: Upload CSV
 st.markdown(""":blue[Upload Data:] Use the file/url uploader on sidebar. Select the target (prediction) column. """)
@@ -28,7 +28,7 @@ uploaded_file = st.sidebar.file_uploader("Upload your input file", type=['csv','
 
 # Input field for URL
 url = st.sidebar.text_input("Or Provide Direct Download URL", value=None, placeholder="Enter URL and press Enter", key='url')
-st.sidebar.write("Example url: https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indians-diabetes.csv")
+st.sidebar.write("Example url: https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data")
 if uploaded_file is not None:
     df,  numeric_cols, categorical_cols, target_column, regression_type = load_dataframe(uploaded_file)
     if df is not None: 
